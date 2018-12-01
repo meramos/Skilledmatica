da0f4cd553e69db3308185e4aec8154d049d0d3b
María E. Ramos Morales <mariaeramosmorales@gmail.com>
2018-12-01 18:13:23 -0400

    ADD: Add awesomebutton to landing page and chance styling

--- src/Home/containers/index.jsx
+++ src/Home/containers/index.jsx
@@ -1,12 +1,15 @@
 import React, { PureComponent } from 'react';
 import { Link } from 'react-router-dom';
+import { AwesomeButton } from 'react-awesome-button';
 
 export default class index extends PureComponent {
   render() {
     return (
       <div style={styles.container}>
-        <p>Landing</p>
-        <Link to="/play">Play</Link>
+        <p style={styles.title}>Skilledmatica</p>
+        <AwesomeButton type="primary">
+          <Link to="/play">Start</Link>
+        </AwesomeButton>
       </div>
     );
   }
@@ -19,5 +22,10 @@ const styles = {
     flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    fontStyle: 'italic',
+    fontSize: '50px',
+    color: '#0D1321',
   },
 };
