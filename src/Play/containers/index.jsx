c1b0902fc3b35dccd55c9f2d73ad9cfa8c2c707a
María E. Ramos Morales <mariaeramosmorales@gmail.com>
2018-12-01 15:42:43 -0400

    ADD: added reset button and keypad

--- src/Play/containers/index.jsx
+++ src/Play/containers/index.jsx
@@ -1,22 +1,29 @@
 import React, { PureComponent } from 'react';
-import { AwesomeButtonProgress } from 'react-awesome-button';
+import { AwesomeButton, AwesomeButtonProgress } from 'react-awesome-button';
 
 import Equation from '../components/Equation';
 import OperatorPanel from './OperatorPanel';
+import KeypadPanel from './KeypadPanel';
 
 export default class Play extends PureComponent {
   constructor(props) {
     super(props);
 
     this.state = {
+      start: '4x + 3 = 2x+1',
       equation: '4x + 3 = 2x+1',
+      solution: 'x = -1',
+      next: undefined,
     };
 
-    this.renderPanel = this.renderPanel.bind(this);
+    this.renderOperatorPanel = this.renderOperatorPanel.bind(this);
+    this.renderKeypadPanel = this.renderKeypadPanel.bind(this);
+    this.submit = this.submit.bind(this);
+    this.reset = this.reset.bind(this);
   }
 
   submit() {
-    if (this.state.value === 6) {
+    if (this.state.equation === this.state.solution) {
       this.setState({
         result: 'CORRECT!',
       });
@@ -27,12 +34,28 @@ export default class Play extends PureComponent {
     }
   }
 
-  renderPanel() {
+  reset() {
+    this.setState({ equation: this.state.start });
+  }
+
+  renderOperatorPanel() {
     const { equation } = this.state;
     return (
       <OperatorPanel
         equation={equation}
         onChangeEquation={equation => {
+          this.setState({ equation });
+        }}
+      />
+    );
+  }
+
+  renderKeypadPanel() {
+    const { equation } = this.state;
+    return (
+      <KeypadPanel
+        equation={equation}
+        onChange={equation => {
           this.setState({ equation });
         }}
       />
@@ -63,8 +86,14 @@ export default class Play extends PureComponent {
             justifyContent: 'center',
           }}
         >
-          {this.renderPanel()}
+          {this.renderOperatorPanel()}
+          {this.renderKeypadPanel()}
+
           <div style={{ marginTop: '10px' }}>
+            <AwesomeButton type="primary" action={this.reset}>
+              reset
+            </AwesomeButton>
+
             <AwesomeButtonProgress
               type="secondary"
               size="medium"
