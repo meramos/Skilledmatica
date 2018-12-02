82de0b6649c9b4dee2a3606174693f85f3a714ea
María E. Ramos Morales <mariaeramosmorales@gmail.com>
2018-12-02 01:12:58 -0400

    RENAMED: exercises.json to exercises.js

---
+++ src/exercises.js
@@ -0,0 +1,22 @@
+export const exercises = {
+  '1': {
+    equation: 'x+5=3',
+    solution: -2,
+  },
+  '2': {
+    equation: '2x-4=8',
+    solution: 6,
+  },
+  '3': {
+    equation: 'x+3=7',
+    solution: 4,
+  },
+  '4': {
+    equation: '(1/2)*x=5',
+    solution: 10,
+  },
+  '5': {
+    equation: '3(x+1)=0',
+    solution: -1,
+  },
+};
